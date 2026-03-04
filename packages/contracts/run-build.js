c5a67df76500406168d6f7ed2833d6a2595c3c7b
Carlos <cdgc.1215@gmail.com>
2026-03-03 20:33:41 -0500

    bbuild fix

--- packages/contracts/run-build.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const path = require("path");
-const { execSync } = require("child_process");
-
-const rootNodeModules = path.resolve(__dirname, "..", "..", "node_modules");
-const env = { ...process.env, NODE_PATH: rootNodeModules };
-
-execSync("npx tsc", { stdio: "inherit", env });
